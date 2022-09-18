6d7ef8196e4d09ed1ce3990dec250ccf2842420a
Johannes <hanruvandermerwe2@gmail.com>
2022-09-18 17:14:04 +0200

    upload

--- indexScript.js
+++ indexScript.js
@@ -1,1 +1,30 @@
-//
+$(document).ready(function (){
+    $('#sideNav').css("display", "grid")
+    $(".pageContainers").fadeOut("slow")
+    $('#bio').fadeIn("slow")
+    $("#selected").text("About")
+    
+    $('#info').click(function(){
+        $('#sideNav').fadeIn("slow")
+        $(this).text("Home")
+    })
+    
+    $('#mainBox').click(function(){
+        $('#sideNav').fadeOut("slow")
+        $("#info").text("Show me")
+    })
+
+    $("#About").click(function(){
+        $(".pageContainers").fadeOut("slow")
+        $('#bio').fadeIn("slow")
+    })
+
+    $("#educationLink").click(function(){
+        $(".pageContainers").fadeOut("slow")
+        $("#education").fadeIn("slow")
+    })
+    $("#experienceLink").click(function(){
+        $(".pageContainers").fadeOut("slow")
+        $("#experience").fadeIn("slow")
+    })
+})
